6b5f6c4f1cf0e62efac57f1094737b1dd7c1dd87
Boris Quiroz <boris@insert-coin.org>
2017-05-02 14:51:14 -0300

    deps and code of patroni

--- provision/postgresql.sh
+++ provision/postgresql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt update
-apt install postgresql etcd haproxy libyaml python
-easy_install pip
+apt install -y etcd haproxy libyaml-0-2 python-pip postgresql-9.6
 pip install psycopg2 pyyaml
-git clone git@github.com:zalando/patroni.git /home/vagrant/patroni
+git clone https://github.com/zalando/patroni.git /home/ubuntu/patroni
 
