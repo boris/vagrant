d72081e03bcdfb3cbf4c2df50acc8173d4e4536e
Boris Quiroz <boris@insert-coin.org>
2021-05-24 08:57:25 -0400

    fix psql client

--- provision/databases.sh
+++ provision/databases.sh
@@ -8,7 +8,7 @@ echo "=> Installing MySQL client"
 sudo apt-get install -y mysql-client
 
 echo "=> Installing PostgreSQL client"
-sudo apt-get install -y postgresql-client-common postgresql-client-10
+sudo apt-get install -y postgresql-client-common postgresql-client
 
 echo "=> Adding custom prompts"
 echo "\set PROMPT1 '%n@%/%R%#%x '" > /home/vagrant/.psqlrc
